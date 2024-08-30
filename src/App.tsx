8c5d628d55e4908bcaa5b3a712baf6a94876b07b
chanwoo <66253711+chanwoo000@users.noreply.github.com>
2024-08-30 17:12:06 +0900

    테스트 완료

--- src/App.tsx
+++ src/App.tsx
@@ -2,35 +2,11 @@
 import React from 'react';
 import styled from 'styled-components';
 import { GlobalStyles } from './styles/GlobalStyles';
-import logo from './assets/Logo.svg';
-import kakao from './assets/Kakao.svg';
-
-const LogoImage = styled.div`
-    margin-top: 150px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`;
-
-const KakaoImage = styled.div`
-    margin-top: 350px;
-    margin-bottom: 100px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`;
 
 const App: React.FC = () => {
     return (
         <>
             <GlobalStyles />
-            <LogoImage>
-                <img src={logo} alt="logo"/>
-            </LogoImage>
-
-            <KakaoImage>
-                <img src={kakao} alt="kakao"/>
-            </KakaoImage>
         </>
     );
 };
