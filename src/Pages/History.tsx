cf9f2613a7e40aaa80ae085c636be9933b8a8c66
MJspace <mjsa02@kookmin.ac.kr>
2024-09-21 03:25:33 +0900

    refactor: 히스토리 및 피팅 결과 페이지 스타일 폴더 구조 수정

--- src/Pages/History.tsx
+++ src/Pages/History.tsx
@@ -1,6 +1,6 @@
 // 코디 추천 결과랑 가상피팅 결과를 조건문 써서 2개 렌더링하기
 import React, { useState } from 'react';
-import styled from 'styled-components';
+import * as H from "../styles/HistoryStyles";
 import TagButton from '../components/Button/TagButton';
 import Header from '../components/header/Header';
 import ClickButton from '../components/Button/ClickButton';
@@ -44,17 +44,17 @@ const History: React.FC<Props> = ({ recommendationData, onClickDelete, currentId
     console.log(data)
 
     return (
-      <InfoWrapper>
-        <InfoBoldText>{categoryName}</InfoBoldText>
+      <H.InfoWrapper>
+        <H.InfoBoldText>{categoryName}</H.InfoBoldText>
         {category?.items.map((item, index) => (
-          <InfoWrapper key={index}>
-            <InfoNormalText>{item.name}</InfoNormalText>
+          <H.InfoWrapper key={index}>
+            <H.InfoNormalText>{item.name}</H.InfoNormalText>
             {item.tags.map((tag, tagIndex) => (
               <TagButton key={tagIndex} name={tag} withHash={true} />
             ))}
-          </InfoWrapper>
+          </H.InfoWrapper>
         ))}
-      </InfoWrapper> 
+      </H.InfoWrapper> 
     );
   };
   //푸터 버튼 이벤트
@@ -65,95 +65,38 @@ const History: React.FC<Props> = ({ recommendationData, onClickDelete, currentId
   return (
     <div>
       <Header text='히스토리' />
-      <Container>
-        <Result />
-        <InfoWrapper>
-          <DateText>
+      <H.Container>
+        <H.Result />
+        <H.InfoWrapper>
+          <H.DateText>
           {data.date}에 추천 받았어요.
-          </DateText>
-          <Delete onClick={() => onClickDelete(data.id)}>삭제</Delete>
-        </InfoWrapper>
-        <InfoContainer>
+          </H.DateText>
+          <H.Delete onClick={() => onClickDelete(data.id)}>삭제</H.Delete>
+        </H.InfoWrapper>
+        <H.InfoContainer>
           {renderCategory('상의')}
           {renderCategory('하의')}
           {renderCategory('기타')}
-        </InfoContainer>
-        <ReviewWrapper>
-          <InfoWrapper>
+        </H.InfoContainer>
+        <H.ReviewWrapper>
+          <H.InfoWrapper>
             <img src={ReviewData.temperature[0].emojiSrc} alt="추웠어요" />
             {ReviewData.temperature[0].label}
-          </InfoWrapper>
-          <InfoWrapper>
+          </H.InfoWrapper>
+          <H.InfoWrapper>
             <img src={ReviewData.likeability[0].emojiSrc} alt="별로예요" />
             {ReviewData.likeability[0].label}
-          </InfoWrapper>
-        </ReviewWrapper>
+          </H.InfoWrapper>
+        </H.ReviewWrapper>
         <ClickButton variant='historyButton' onClick={handleClick}>
          완료
         </ClickButton>
         <ClickButton variant='reviewButton' onClick={handleClick}>
          리뷰 기록하기
         </ClickButton>
-      </Container>
+      </H.Container>
     </div>
   );
 };
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin: 20px 25px 25px 25px;
-  font-family: Pretendard;
-`;
-
-const Result = styled.div`
-  margin: 0px 0px 17px 0px;
-  border-radius: 10px;
-  height: 432px;
-  background-color : var(--gray--2);
-`;
-
-const DateText = styled.div`
-  font-size: 18px;
-  font-weight: 600;
-`;
-
-const InfoBoldText = styled.div`
-  font-size: 13px;
-  font-weight: 600;
-  margin-right: 20px;
-`;
-
-const InfoNormalText = styled.div`
-  font-size: 10px;
-  font-weight: 500;
-`;
-
-const InfoContainer = styled.div` //정보 담은 컨테이너
-  display:flex;
-  margin-top: 24px;
-  flex-direction: column;
-  gap: 15px;
-`;
-
-const InfoWrapper = styled.div` //태그 정보 한줄 씩
-  display: flex;
-  gap: 4px;
-  align-items: center;
-`;
-
-const Delete = styled.div`
-  color: var(--black);
-  font-size: 14px;
-  font-weight: 500;
-  margin-left: 65px;
-  cursor: pointer;
-`;
-
-const ReviewWrapper = styled.div`
-  display: flex;
-  gap: 20px;
-  margin: 28px 0px;
-`;
 
 export default History;
--- src/Pages/ShowResult.tsx
+++ src/Pages/ShowResult.tsx
@@ -1,6 +1,6 @@
 // 코디 추천 결과랑 가상피팅 결과를 조건문 써서 2개 렌더링하기
 import React from 'react';
-import styled from 'styled-components';
+import * as S from "../styles/ShowResultStyles";
 import TagButton from '../components/Button/TagButton';
 import Header from '../components/header/Header';
 import ClickButton from '../components/Button/ClickButton';
@@ -25,6 +25,7 @@ interface Props {
   currentId: number;
 }
 
+//현재 태그 정보 데이터는 이전 페이지에서 props로 받고 있고, 리뷰결과는 바로 더미데이터 페이지에서 받아오고 있음. 뭐가 더 좋을까
 const ShowResult: React.FC<Props> = ({ recommendationData, fittingData, isVirtualFitting, currentId }) => {
   const data = isVirtualFitting ? fittingData : recommendationData;
 
@@ -40,17 +41,17 @@ const ShowResult: React.FC<Props> = ({ recommendationData, fittingData, isVirtua
     const category = Id?.categories.find(category => category.category === categoryName);
 
     return (
-      <InfoWrapper>
-        <InfoBoldText>{categoryName}</InfoBoldText>
+      <S.InfoWrapper>
+        <S.InfoBoldText>{categoryName}</S.InfoBoldText>
         {category?.items.map((item, index) => (
-          <InfoWrapper key={index}>
-            <InfoNormalText>{item.name}</InfoNormalText>
+          <S.InfoWrapper key={index}>
+            <S.InfoNormalText>{item.name}</S.InfoNormalText>
             {item.tags.map((tag, tagIndex) => (
               <TagButton key={tagIndex} name={tag} withHash={true} />
             ))}
-          </InfoWrapper>
+          </S.InfoWrapper>
         ))}
-      </InfoWrapper>
+      </S.InfoWrapper>
     );
   };
 
@@ -61,17 +62,17 @@ const ShowResult: React.FC<Props> = ({ recommendationData, fittingData, isVirtua
   return (
     <div>
       <Header text={isVirtualFitting ? '가상 피팅 결과' : '코디 추천 결과'} />
-      <Container>
-        <Result />
-        <DateText>
+      <S.Container>
+        <S.Result />
+        <S.DateText>
         {Id.date}에 추천 받았어요.
-        </DateText>
-        <InfoContainer>
+        </S.DateText>
+        <S.InfoContainer>
           {renderCategory('상의')}
           {renderCategory('하의')}
           {renderCategory('기타')}
-        </InfoContainer>
-      </Container>
+        </S.InfoContainer>
+      </S.Container>
       {!isVirtualFitting && (
       <ClickButton variant='footerButton' onClick={handleClick}>
         코디 추천 결과
@@ -81,48 +82,5 @@ const ShowResult: React.FC<Props> = ({ recommendationData, fittingData, isVirtua
 
   );
 };
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin: 20px 25px 25px 25px;
-  font-family: Pretendard;
-`;
-
-const Result = styled.div`
-  margin: 0px 0px 17px 0px;
-  border-radius: 10px;
-  height: 432px;
-  background-color : var(--gray--2);
-`;
-
-const DateText = styled.div`
-  font-size: 18px;
-  font-weight: 600;
-`;
-
-const InfoBoldText = styled.div`
-  font-size: 13px;
-  font-weight: 600;
-  margin-right: 20px;
-`;
-
-const InfoNormalText = styled.div`
-  font-size: 10px;
-  font-weight: 500;
-`;
-
-const InfoContainer = styled.div` //정보 담은 컨테이너
-  display:flex;
-  margin-top: 24px;
-  flex-direction: column;
-  gap: 15px;
-`;
-
-const InfoWrapper = styled.div` //정보 한줄 씩
-  display: flex;
-  gap: 4px;
-`;
-
 
 export default ShowResult;
