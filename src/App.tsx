d91bc356232337f8fbe677b44e0eb687bac894e9
chanwoo <chanwoo0607@naver.com>
2024-09-22 21:08:49 +0900

    feat : 라우터 1차 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,44 @@
 // src/App.tsx
 import React from 'react';
-import styled from 'styled-components';
 import { GlobalStyles } from './styles/GlobalStyles';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import MainPage from './pages/MainPage/index';
-import KakaoCallback from "./pages/KakaoCallback";
-import RegisterPage from "./pages/Register";
-import OnboardingPage from "./pages/Onboarding";
+// import MainPage from './pages/MainPage/index';
+// import KakaoCallback from "./pages/KakaoCallback";
+// import RegisterPage from "./pages/Register";
+// import OnboardingPage from "./pages/Onboarding/Onboarding";
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Onboarding from "./pages/Onboarding/Onboarding"
-import or from "./pages/OutfitRecommendations/OutfitRecommendations"
-import OutfitRecommendations from "./pages/OutfitRecommendations/OutfitRecommendations";
-import VirtualFitting from "./pages/VirtualFitting/VirtualFitting";
+import Onboarding from "./Pages/Onboarding/Onboarding"
+import OutfitRecommendations from "./Pages/OutfitRecommendations/OutfitRecommendations";
+import VirtualFitting from "./Pages/VirtualFitting/VirtualFitting";
 const App: React.FC = () => {
   return (
       <>
         <GlobalStyles />
           <BrowserRouter>
               <Routes>
-                  <Route path='/' element={<Onboarding />} />
+                  <Route path='/onboarding' element={<Onboarding />} />
                   <Route path='/or' element={<OutfitRecommendations/>}/>
                   <Route path='/vf' element={<VirtualFitting/>}/>
               </Routes>
           </BrowserRouter>
       </>
-      <div className="App">
-        <BrowserRouter>
-          <GlobalStyles />
-          <Routes>
-            <Route path="/" element={<MainPage/>}/>
-            <Route path="/callback" element={<KakaoCallback/>}/>
-            <Route path="/onboarding" element={<OnboardingPage/>}/>
-            <Route path="/register/:type" element={<RegisterPage/>}/>
-            <Route path="/coordination" element={<MainPage/>}/> // 코디 추천
-            <Route path="/virtual-fitting" element={<MainPage/>}/> // 가상 피팅
-            <Route path="/closet" element={<MainPage/>}/> // 옷장
-            <Route path="/wish" element={<MainPage/>}/> // 위시
-            <Route path="/history" element={<MainPage/>}/> // 히스토리
-          </Routes>
-        </BrowserRouter>
-
-      </div>
+      // <div className="App">
+      //   <BrowserRouter>
+      //     <GlobalStyles />
+      //     <Routes>
+      //       <Route path="/" element={<MainPage/>}/>
+      //       <Route path="/callback" element={<KakaoCallback/>}/>
+      //       <Route path="/onboarding" element={<OnboardingPage/>}/>
+      //       <Route path="/register/:type" element={<RegisterPage/>}/>
+      //       <Route path="/coordination" element={<MainPage/>}/> // 코디 추천
+      //       <Route path="/virtual-fitting" element={<MainPage/>}/> // 가상 피팅
+      //       <Route path="/closet" element={<MainPage/>}/> // 옷장
+      //       <Route path="/wish" element={<MainPage/>}/> // 위시
+      //       <Route path="/history" element={<MainPage/>}/> // 히스토리
+      //     </Routes>
+      //   </BrowserRouter>
+      // </div>
   );
 };
 
