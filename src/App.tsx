ba30308bc1f64b98c266490fce78795ce23da589
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2024-09-22 18:39:43 +0900

    feat: kakao 소셜 로그인 콜백 페이지 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import { GlobalStyles } from './styles/GlobalStyles';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import MainPage from './pages/MainPage/index';
-
+import JoinPage from "./pages/Join";
+import KakaoCallback from "./pages/KakaoCallback";
+import RegisterPage from "./pages/Register";
 
 const App: React.FC = () => {
   return (
@@ -12,6 +14,9 @@ const App: React.FC = () => {
           <GlobalStyles />
           <Routes>
             <Route path="/" element={<MainPage/>}/>
+            <Route path="/callback" element={<KakaoCallback/>}/>
+            <Route path="/join" element={<JoinPage/>}/>
+            <Route path="/register/:type" element={<RegisterPage/>}/>
             <Route path="/coordination" element={<MainPage/>}/> // 코디 추천
             <Route path="/virtual-fitting" element={<MainPage/>}/> // 가상 피팅
             <Route path="/closet" element={<MainPage/>}/> // 옷장
