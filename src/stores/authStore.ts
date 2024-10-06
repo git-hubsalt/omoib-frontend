1f1f0a6daf213a156425eea8e0c76d2471e52a54
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2024-10-07 00:34:28 +0900

    feat: 로그인 상태관리 생성 및 axios 인스턴스 생성

---
+++ src/stores/authStore.ts
@@ -0,0 +1,25 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware/persist';
+
+interface AuthState {
+  isLoggedIn: boolean;
+  accessToken: string | null;
+  login: (accessToken: string) => void;
+  logout: () => void;
+}
+
+const useAuthStore = create<AuthState>()(
+  persist<AuthState>(
+    set => ({
+      isLoggedIn: false,
+      accessToken: null,
+      login: (accessToken: string) => set({ isLoggedIn: true, accessToken }),
+      logout: () => set({ isLoggedIn: false, accessToken: null }),
+    }),
+    {
+      name: 'authStore',
+    }
+  ),
+);
+
+export default useAuthStore;
