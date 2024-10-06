8b22262b5419a8c71ea5fe71d9b5e384fda71b80
daun-up <awhekdns@kookmin.ac.kr>
2024-10-06 22:45:36 +0900

    feature: back 버튼 기능 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
-import { ReactComponent as Back  } from '../../assets/back.svg'
-import {HeaderContainer, BackWrapper, HeaderText, DummyBox} from './HeaderStyle';
+import { ReactComponent as Back } from '../../assets/back.svg';
+import { HeaderContainer, BackWrapper, HeaderText, DummyBox } from './HeaderStyle';
+import { useNavigate } from 'react-router-dom'; // 추가
 
 type HeaderProps = {
   text: string;
 };
 
 const Header = ({ text }: HeaderProps) => {
+  const navigate = useNavigate(); // useNavigate 훅 사용
+
+  const handleBackClick = () => {
+    navigate(-1); // 이전 페이지로 이동
+  };
+
   return (
     <HeaderContainer>
-      <BackWrapper><Back/></BackWrapper>
+      <BackWrapper onClick={handleBackClick}>
+        <Back />
+      </BackWrapper>
       <HeaderText>{text}</HeaderText>
       <DummyBox></DummyBox>
     </HeaderContainer>
