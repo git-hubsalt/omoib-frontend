1f99504a70c327c6f259e39d8b7fe5c64f3c4136
daun-up <awhekdns@kookmin.ac.kr>
2024-10-06 15:29:57 +0900

    feature: 데이터 추가

--- src/components/Button/TagButton.tsx
+++ src/components/Button/TagButton.tsx
@@ -1,18 +1,39 @@
 import styled from 'styled-components';
+import { useState } from 'react';
 
 type ButtonProps = {
     className?: string;
     name: string;
     withHash?: boolean;
+    onClick?: () => void;
 };
 
-const TagButton = ({ className, name, withHash = true }: ButtonProps) => {
+const TagButton = ({ className, name, withHash = true, onClick }: ButtonProps) => {
+    const [selected, setSelected] = useState<boolean>(false);
     const tagContent = withHash ? `#${name}` : name; // withHash가 true이면 # 추가
-    const { backgroundColor, color } = getTagStyles(name); // 태그의 기본 색상
+    const { backgroundColor, color } = getTagStyles(name); // getTagStyles로 지정된 색상 불러오기
+    const defaultTextColor = '#989898'; // 기본 텍스트 색상
+
+    // 클릭 핸들러
+    const handleTagClick = () => {
+        if (!withHash) { // withHash가 true일 때 클릭 이벤트가 작동하지 않게 설정
+            setSelected((current) => !current);
+            if (onClick) {
+                onClick();
+            }
+        }
+    };
 
     return (
-      <TagBox className={className} backgroundColor={backgroundColor} withHash={withHash}>
-          <StyledTag color={color}>{tagContent}</StyledTag>
+      <TagBox
+        className={className}
+        backgroundColor={withHash ? backgroundColor : selected ? backgroundColor : '#F4F4F4'}
+        withHash={withHash}
+        onClick={handleTagClick}
+      >
+          <StyledTag color={withHash ? color : selected ? color : defaultTextColor}>
+              {tagContent}
+          </StyledTag>
       </TagBox>
     );
 };
@@ -43,22 +64,23 @@ const getTagStyles = (tagContent: string) => {
 
 // withHash에 따라 width를 동적으로 설정
 const TagBox = styled.div<{ backgroundColor: string; withHash: boolean }>`
-    display: flex;
-    border: none;
-    align-items: center;
-    justify-content: center;
-    border-radius: 20px;
-    background-color: ${(props) => props.backgroundColor};
-    min-width: ${(props) => (props.withHash ? '32px' : '50px')};
-    max-width: ${(props) => (props.withHash ? '32px' : '50px')}; 
-    min-height: ${(props) => (props.withHash ? '16px' : '30px')};
-    max-height: ${(props) => (props.withHash ? '16px' : '30px')};
+  display: flex;
+  border: none;
+  align-items: center;
+  justify-content: center;
+  border-radius: 20px;
+  background-color: ${(props) => props.backgroundColor}; // 배경색 동적 적용
+  min-width: ${(props) => (props.withHash ? '32px' : '50px')};
+  max-width: ${(props) => (props.withHash ? '32px' : '50px')};
+  min-height: ${(props) => (props.withHash ? '18px' : '30px')};
+  max-height: ${(props) => (props.withHash ? '18px' : '30px')};
+  cursor: ${(props) => (props.withHash ? 'default' : 'pointer')}; // withHash가 true일 때 클릭 불가능
 `;
 
 const StyledTag = styled.div<{ color: string }>`
   font-size: 10px;
   font-weight: 500;
-  color: ${(props) => props.color};
+  color: ${(props) => props.color}; // 텍스트 색상 동적 적용
 `;
 
 export default TagButton;
