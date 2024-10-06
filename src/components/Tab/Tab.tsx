9dc86226de26ff9a8b46e124d47d7acc410f14f4
chanwoo <chanwoo0607@naver.com>
2024-10-06 22:53:09 +0900

    refactor : 코드 수정

--- src/components/Tab/Tab.tsx
+++ src/components/Tab/Tab.tsx
@@ -2,28 +2,25 @@ import React, { useState } from 'react';
 import * as style from "./TabStyle";
 
 interface TabProps {
-    tabs: string[];
+  tabs: string[];
 }
 
 const TabComponent: React.FC<TabProps> = ({ tabs }) => {
-    const [activeTab, setActiveTab] = useState<number>(0);
+  const [activeTab, setActiveTab] = useState<number>(0);
 
-    return (
-        <style.TabContainer>
-            <style.SliderWrapper>
-                <style.Slider activeTab={activeTab} />
-            </style.SliderWrapper>
-            {tabs.map((tab, index) => (
-                <style.TabButton
-                    key={tab}
-                    active={activeTab === index}
-                    onClick={() => setActiveTab(index)}
-                >
-                    {tab}
-                </style.TabButton>
-            ))}
-        </style.TabContainer>
-    );
+  return (
+    <style.TabLayout>
+      {tabs.slice(0, 2).map((tab, index) => (
+        <style.TabButton
+          key={tab}
+          active={activeTab === index}
+          onClick={() => setActiveTab(index)}
+        >
+          {tab}
+        </style.TabButton>
+      ))}
+    </style.TabLayout>
+  );
 };
 
 export default TabComponent;
--- src/components/Tab/TabStyle.ts
+++ src/components/Tab/TabStyle.ts
@@ -1,47 +1,30 @@
 import styled from "styled-components";
 
 // 탭 컨테이너 스타일
-export const TabContainer = styled.div`
+export const TabLayout = styled.div`
   width: 270px;
   height: 40px;
   border-radius: 10px;
   display: flex;
+  justify-content: space-between;
   background-color: #F2F3F5;
   position: relative;
   margin: 30px auto;
-`;
-
-// 슬라이더 래퍼
-export const SliderWrapper = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  border-radius: 10px;
-  overflow: hidden;
-`;
-
-// 슬라이더
-export const Slider = styled.div<{ activeTab: number }>`
-  width: 50%;
-  height: 100%;
-  background-color: #89CEFA;
-  border-radius: 10px;
-  transform: translateX(${({ activeTab }) => activeTab * 100}%);
-  transition: transform 0.3s ease;
+  gap: 5px;
 `;
 
 // 버튼 스타일
 export const TabButton = styled.button<{ active: boolean }>`
-  width: 50%;
-  height: 100%;
+  width: 50%; // 두 개의 버튼이니까 넓이를 50%로 조정
+  height: 90%;
+  margin: auto;
   border: none;
-  background-color: transparent;
-  color: ${({ active }) => (active ? '#ffffff' : '#808080')};
+  background-color: ${({ active }) => (active ? '#89CEFA' : 'transparent')}; // 선택된 탭이면 노란색 배경, 아니면 투명
+  color: ${({ active }) => (active ? '#ffffff' : '#808080')}; // 선택된 탭이면 흰색 글씨, 아니면 회색 글씨
   font-size: 14px;
   cursor: pointer;
+  border-radius: 10px;
   z-index: 1;
   position: relative;
-  transition: color 0.3s ease;
+  transition: background-color 0.3s ease;
 `;
