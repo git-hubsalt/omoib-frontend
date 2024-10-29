1e49ee913756bd589f88076d5ee51b97f907f67f
daun-up <awhekdns@kookmin.ac.kr>
2024-10-29 14:47:12 +0900

    feat: dummy data 처리

--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -3,18 +3,22 @@ import Header from '../../components/Header/Header';
 import { LogoutWrapper } from './style';
 import { InfoWrapper } from './style';
 
-export default function MyPage() {
 
+export default function MyPage() {
+  const data = {
+    nickname: '조다운',
+    email: 'awhekdns@kookmin.ac.kr',
+  }
   return (
     <div>
       <Header text="마이페이지" />
       <InfoWrapper>
         <div>닉네임</div>
-        <div>조다운</div>
+        <div>{data.nickname}</div>
       </InfoWrapper>
       <InfoWrapper>
         <div>이메일</div>
-        <div>awhekdns@kookmin.ac.kr</div>
+        <div>{data.email}</div>
       </InfoWrapper>
       <InfoWrapper>
         <div>신체 사진</div>
