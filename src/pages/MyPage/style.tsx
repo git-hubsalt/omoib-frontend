7efdfc1b611e889207167226814911e586470d5e
daun-up <awhekdns@kookmin.ac.kr>
2024-10-29 11:28:10 +0900

    refactor: 코드 포맷팅

---
+++ src/pages/MyPage/style.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const InfoWrapper = styled.div`
+    padding: 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+export const LogoutWrapper = styled.div`
+    padding: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    font-size: 14px;
+`;
