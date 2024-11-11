9a20e0db4987d3d3a7d8d6fdea01055deed9e19f
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2024-11-11 18:00:01 +0900

    feat: 알림 api 연결

--- src/components/NoticeItem/index.tsx
+++ src/components/NoticeItem/index.tsx
@@ -6,13 +6,20 @@ import {
   MainText,
   SubText,
 } from './style';
+import {useNavigate} from "react-router-dom";
 
-export default function Notice() {
+interface NoticeItemProps {
+  timestamp: string;
+  historyId: number;
+}
+
+export default function NoticeItem({ timestamp, historyId }: NoticeItemProps) {
+  const navigate = useNavigate();
   return (
-    <NoticeContainer>
+    <NoticeContainer onClick={() => navigate(`/history/${historyId}`)}>
       <BellIcon />
       <NoticeTextContainer>
-        <MainText>17:30 에 시작한 코디 추천이 끝났습니다.</MainText>
+        <MainText>{timestamp}에 시작한 코디 추천이 끝났습니다.</MainText>
         <SubText>지금 결과를 확인해보세요!</SubText>
       </NoticeTextContainer>
     </NoticeContainer>
