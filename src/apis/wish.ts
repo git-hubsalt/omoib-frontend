40928a378a3ead081f05c8e49d2a195241412001
chanwoo <chanwoo0607@naver.com>
2024-11-11 19:01:44 +0900

    refactor : wish api 수정

--- src/apis/wish.ts
+++ src/apis/wish.ts
@@ -1,5 +1,25 @@
 import { privateFormDataAxiosInstance } from './axiosInstance';
+import {ClothesBase} from "../types/type";
 
 export const deleteWish = async (Id: number) => {
   return await privateFormDataAxiosInstance.delete(`/wish/${Id}`);
 };
+
+
+export const postWish = (clothes: ClothesBase, images: File[]) => {
+  const formData = new FormData();
+  const requestBody = { clothes: clothes };
+
+  formData.append(
+    'requestDTO',
+    new Blob([JSON.stringify(requestBody)], { type: 'application/json' }),
+  )
+
+  images.forEach(image => {
+    if (image.size > 0) {
+      formData.append('image', image);
+    }
+  });
+
+  return privateFormDataAxiosInstance.post('/wish', formData);
+}
